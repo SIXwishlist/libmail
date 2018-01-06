721a2afb88bf634d2029bdc2896c04400be84daa
dpress29 <dpressense@protonmail.com>
2018-01-06 22:54:02 +0100

    migration to bootstrap

--- index.php
+++ index.php
@@ -3,54 +3,33 @@
 <?php include("includes/head.php"); ?>
 
 <?php include("includes/header.php"); ?>
-    <main>
-      <div class="section no-pad-bot" id="index-banner">
-        <div class="container">
-          <br>
-          <h1 class="header center teal-text">Libmail</h1>
-          <div class="row center">
-            <h5 class="header col s12 light">Un service de courrier électronique simple, rapide, libre et respectueux de la vie privée.</h5>
-          </div>
-          <div class="row center">
-            <a href="user.php" id="download-button" class="btn-large waves-effect waves-light">inscription</a>
-          </div>
+    <div class="container">
+      <div class="jumbotron text-center">
+        <h1 class="display-4 text-primary">Libmail</h1>
+        <h2 class="">Un service de courrier électronique simple, rapide, libre et respectueux de la vie privée.</h2>
+        <br>
+        <a href="user.php" class="btn btn-lg btn-primary">inscription</a>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 text-center padding">
+          <h1 class="text-primary display-3"><span class="oi oi-flash" title="flash" aria-hidden="true"></span></h1>
+          <h2>Facile et Performant</h2>
+          <p class="text-justify">Nous proposons un service mail (imap, imaps, pop, pops, smtp) configurable avec tous les clients de messagerie standards, ainsi qu'une interface roundcube pour consulter votre messagerie directement depuis votre navigateur web.</p>
+        </div>
+        <div class="col-md-4 text-center padding">
+          <h1 class="text-primary display-3"><span class="oi oi-people" title="people" aria-hidden="true"></span></h1>
+          <h2>Un Projet Communautaire</h2>
+          <p class="text-justify">La démarche est résolument militante car aujourd'hui il est très difficile de trouver un service de courrier électronique indépendant et fonctionnant uniquement avec des logiciels libres. Nous comptons également sur la communauté pour améliorer notre service.</p>
+        </div>
+        <div class="col-md-4 text-center padding">
+          <h1 class="text-primary display-3"><span class="oi oi-wrench" title="wrench" aria-hidden="true"></span></h1>
+          <h2>Une conception solide</h2>
+          <p class="text-justify">Les infrastructures sont basées sur un VPS hébergé chez OVH. Le serveur fonctionne avec le système d'exploitation Debian 9, connu pour sa stabilité. Les données stockées sont chiffrées pour ganrantir la sécurité et la confidentialité.</p>
         </div>
       </div>
-      <div class="container">
-        <div class="section">
-          <div class="row">
-
-            <div class="col s12 m4">
-              <div class="icon-block">
-                <h2 class="center light-blue-text large"><i class="material-icons">flash_on</i></h2>
-                <h5 class="center">Facile et Performant</h5>
-
-                <p class="light">Nous proposons un service mail (imap, imaps, pop, pops, smtp) configurable avec tous les clients de messagerie standards, ainsi qu'une interface roundcube pour consulter votre messagerie directement depuis votre navigateur web.</p>
-              </div>
-            </div>
-
-            <div class="col s12 m4">
-              <div class="icon-block">
-                <h2 class="center light-blue-text large"><i class="material-icons">group</i></h2>
-                <h5 class="center">Un projet totalement libre !</h5>
-
-                <p class="light">La démarche est résolument militante car aujourd'hui il est très difficile de trouver un service de courrier électronique indépendant et fonctionnant uniquement avec des logiciels libres. Nous comptons également sur la communauté pour améliorer notre service.</p>
-              </div>
-            </div>
-
-            <div class="col s12 m4">
-              <div class="icon-block">
-                <h2 class="center light-blue-text large"><i class="material-icons">settings</i></h2>
-                <h5 class="center">Une conception solide</h5>
-
-                <p class="light">Les infrastructures sont basées sur un VPS hébergé chez OVH. Le serveur fonctionne avec le système d'exploitation Debian 9, connu pour sa stabilité. Les données stockées sont chiffrées pour ganrantir la sécurité et la confidentialité.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
-    <br />
+    </div>
   <?php include("includes/footer.php"); ?>
   </body>
 </html>
