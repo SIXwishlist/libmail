46da85b5d93ced6f2af924a6ea54415300473c1a
dpress29 <dpressense@protonmail.com>
2018-01-06 22:47:04 +0100

    migration to bootstrap

--- includes/head.php
+++ includes/head.php
@@ -1,22 +1,33 @@
   <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-
+    <meta charset="utf-8">
     <title>Libmail, Service Mail Libre et Respectueux de la vie Privée</title>
-
-    <link href="css/materialize.min.css" type="text/css" rel="stylesheet" media="all"/>
-    <link href="css/style.css" type="text/css" rel="stylesheet" media="all"/>
-    <link href="css/material-icons.css" type="text/css" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="icon" type="image/png" href="includes/favicon.png">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link href="open-iconic/font/css/open-iconic-bootstrap.css" rel="stylesheet">
+    <style media="screen">
+      html {
+        position: relative;
+        min-height: 100%;
+      }
+      body {
+        padding-top: 4.5rem; /*for navbar*/
+        padding-bottom: 60px; /*for footer*/
+      }
+      .footer {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        height: 60px;
+        line-height: 60px;
+        background-color: #616161;
+      }
+      .padding {
+        padding: 2rem;
+      }
+    </style>
   </head>
-
   <body>
-    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script type="text/javascript" src="js/materialize.min.js"></script>
-    <script>
-      jQuery(document).ready(function($){
-      $(".button-collapse").sideNav();
-      });
-    </script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
