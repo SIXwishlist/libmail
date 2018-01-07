2c66544364385de3ffc8b921bb0ca890ef7aaea8
dpress29 <dpressense@protonmail.com>
2018-01-07 16:57:45 +0100

    migration to bootstrap

--- user.php
+++ user.php
@@ -5,10 +5,8 @@ $securimage = new Securimage();?>
 <!DOCTYPE html>
 <html>
 <?php include("includes/head.php");
-include("includes/header.php"); ?>
-    <main>
-<?php
-include('includes/bdd/bdd.php');
+include("includes/header.php");
+include("includes/bdd/bdd.php");
 $securimage = new Securimage();
 
 if(isset($_POST['email']) && isset($_POST['identity']) && ($_POST['password']) && isset($_POST['password1']) && isset($_POST['captchaText'])){
@@ -43,113 +41,89 @@ if(isset($_POST['email']) && isset($_POST['identity']) && ($_POST['password']) &
     $req1->execute(array('identity' => $_POST['identity'], 'email' => $_POST['email']));
   ?>
 
-      <div class="row">
-        <div class="col s10 offset-s1 m8 offset-m2 l6 offset-l3 center">
-          <h3 class="teal-text">INSCRIPTION À LIBMAIL TERMINÉE</h3>
-          <p>Connectez-vous dès maintenant sur roundcube</p>
-          <a href="https://web.libmail.eu" class="btn-large waves-effect waves-light">C'est parti !</a>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-10 offset-md-2 text-center">
+            <h3 class="text-success">INSCRIPTION À LIBMAIL TERMINÉE</h3>
+            <p>Connectez-vous dès maintenant sur roundcube</p>
+            <a href="https://web.libmail.eu" class="btn btn-lg btn-success">C'est parti !</a>
+          </div>
         </div>
       </div>
 
 <?php  }else{ ?>
 
-      <div class="row">
-        <div class="col s10 offset-s1 m8 offset-m2 l6 offset-l3 center">
-          <h3 class="red-text">INSCRIPTION À LIBMAIL ÉCHOUÉE</h3>
-          <p>L'email demandé existe déjà ou une des informations renseignées ne répond pas aux normes techniques. Veuillez réessayer ...</p>
-          <a href="user.php" class="btn-large waves-effect waves-light">Retour</a>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-10 offset-md-2 text-center">
+            <h3 class="text-danger">INSCRIPTION À LIBMAIL ÉCHOUÉE</h3>
+            <p>L'email demandé existe déjà ou une des informations renseignées ne répond pas aux normes techniques. Veuillez réessayer ...</p>
+            <a href="user.php" class="btn btn-lg btn-success">Retour</a>
+          </div>
         </div>
       </div>
 
 <?php } }else{ ?>
 
-      <script type="text/javascript" src="includes/form_javascript.js"></script>
-      <div class="row">
-        <div class="col s10 offset-s1 m8 offset-m2 l6 offset-l3 center">
-          <br>
-          <h3 class="teal-text">INSCRIPTION À LIBMAIL</h3>
-          <br><br>
-          <form  name="registerForm" action="user.php" method="post">
-            <div class="input-field col s12">
-              <i class="material-icons prefix">send</i>
-              <input placeholder="sous la forme : user@libmail.eu" id="email" type="text" name="email" onchange='emailCheck();'>
-              <label for="email" data-error="L'adresse Libmail renseignée ne convient pas.">Votre adresse Libmail</label>
-            </div>
-            <div class="input-field col s12">
-              <i class="material-icons prefix">account_circle</i>
-              <input id="identity" type="text" name="identity" onchange='identityCheck();'>
-              <label for="identity" data-error="L'identité renseignée ne convient pas.">Nom et Prénom</label>
-            </div>
-            <div class="input-field col s12">
-              <i class="material-icons prefix">vpn_key</i>
-              <input id="password" type="password" name="password" onchange='passwordCheck();'>
-              <label for="password" data-error="Votre mot de passe doit faire au moins 8 caractères.">Mot de passe</label>
-            </div>
-            <div class="input-field col s12">
-              <i class="material-icons prefix">vpn_key</i>
-              <input id="confirmPassword" type="password" name="password1" onchange='confirmPasswordCheck();'>
-              <label for="confirmPassword" data-error="Les mots de passe ne correspondent pas.">Confirmation</label>
-            </div>
-            <div class="col s8 offset-s2">
-              <div class="card-panel grey lighten-3">
-                <input type="checkbox" id="captchaCheckbox" onclick="captchaOpen();"/>
-                <label for="captchaCheckbox">Je ne suis pas un robot.</label>
-                <script type="text/javascript">
-                  function captchaOpen(){
-                    $('#modal1').modal('open');
-                    document.getElementById("captchaCheckbox").checked = false;
-                  }
-                </script>
+  <script type="text/javascript" src="includes/form_javascript.js"></script>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 offset-md-2 col-lg-6 offset-lg-3">
+        <h1 class="text-primary text-center">INSCRIPTION À LIBMAIL</h3>
+        <br><br>
+        <form  name="registerForm" action="user.php" method="post">
+          <div class="form-group col-12">
+            <label for="email" data-error="L'adresse Libmail renseignée ne convient pas.">Votre adresse Libmail</label>
+            <div class="input-group">
+              <input type="text" name="email" class="form-control" id="email" placeholder="Nom d'utilisateur" aria-label="email" aria-describedby="domain" onchange='emailCheck();'>
+              <div class="input-group-append">
+                <span class="input-group-text" id="domain">@libmail.eu</span>
               </div>
             </div>
-            <!-- modal du captcha -->
-            <div id="modal1" class="modal">
-              <div class="row">
-                <div class="col s10 offset-s1 m8 offset-m2 l6 offset-l3 center">
-                  <br>
-                  <h3 class="teal-text">VÉRIFICATION CAPTCHA</h3>
-                  <br><br>
-                  <div class="col s12 center">
-                    <img class="center" id="captcha" src="securimage/securimage_show.php" alt="CAPTCHA Image" />
-                  </div>
-                  <div class="input-field col s12">
-                    <i class="material-icons prefix">security</i>
-                    <input placeholder="" id="captcha" type="text" name="captchaText" onchange='captchaCheck();' maxlength="6">
-                    <label for="captchaText" data-error="Le code renseigné ne convient pas.">Entrer le texte visible ci-dessus.</label>
-                  </div>
-                  <div class="input-field col s12 center">
-                    <p>En cliquant sur ce bouton vous acceptez les <a href="cgu.php" target="_blank">conditions génerales d'utilisation</a></p>
-                    <button class="btn waves-effect waves-light green" type="submit" name="submitActionBt" disabled=""><i class="material-icons left">done_all</i>S'inscrire</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <script type="text/javascript">
-              $(document).ready(function(){
-                $('.modal').modal();
-              });
-            </script>
-          </form>
-        </div>
-        <div class="col s10 offset-s1 m8 offset-m2 l4 offset-l4">
-          <br>
-          <div class="card grey darken-1">
-            <div class="card-content white-text">
-              <span class="card-title">À propos des données personnelles ...</span>
+          </div>
+          <div class="form-group col-12">
+            <label for="identity" data-error="L'identité renseignée ne convient pas.">Nom et Prénom</label>
+            <input type="text" name="identity" class="form-control" id="identity" placeholder="Renseignez votre identité" onchange='identityCheck();'>
+          </div>
+          <div class="form-group col-12">
+            <label for="password" data-error="Votre mot de passe doit faire au moins 8 caractères.">Mot de Passe</label>
+            <input type="password" name="password" class="form-control" id="password" placeholder="Entrez votre Mot de Passe" onchange='passwordCheck();'>
+          </div>
+          <div class="form-group col-12">
+            <label for="confirmPassword" data-error="Les mots de passe ne correspondent pas.">Confirmation de Mot de Passe</label>
+            <input type="password" name="password1" class="form-control" id="confirmPassword" placeholder="Confirmez votre Mot de Passe" onchange='confirmPasswordCheck();'>
+          </div>
+          <div class="form-group col-12 text-center">
+            <img class="center" id="captcha" src="securimage/securimage_show.php" alt="CAPTCHA Image" />
+          </div>
+          <div class="form-group col-12">
+            <label for="captchaText" data-error="Le code renseigné ne convient pas.">Captcha</label>
+            <input type="text" name="captchaText" class="form-control" id="captcha" placeholder="Entrer le texte visible ci-dessus." onchange='captchaCheck();' maxlength="6">
+          </div>
+          <div class="form-group col-12 text-center">
+            <p>En cliquant sur ce bouton vous acceptez les <a href="cgu.php" target="_blank">conditions génerales d'utilisation</a></p>
+            <button class="btn btn-lg btn-primary" type="submit" name="submitActionBt" disabled="">S'inscrire</button>
+          </div>
+          <div class="form-group col-lg-10 offset-lg-1">
+            <div class="card text-white" style="background-color: #616161">
+              <div class="card-body">
+                <h4 class="card-title">À propos des données personnelles ...</h4>
                 <li>Les informations recueillies sur ce formulaire sont enregistrées dans un fichier informatisé par Libmail pour le fonctionnement de vos services mail.</li>
                 <li>Elles sont conservées pendant toute la durée de validité de votre compte Libmail et ne sont destinées à aucune entité tierce.</li>
                 <li>Conformément à la loi « informatique et libertés », vous pouvez exercer votre droit d'accès aux données vous concernant et les faire rectifier en contactant : L'équipe Libmail <a class="orange-text lighten-2" href="mailto:team@libmail.eu?subject=Question_relative_aux_données_personelles">team@libmail.eu</a>.</li>
-            </div>
-            <div class="card-action">
-              <a class="white-text" href="https://www.cnil.fr/" target="_blank">Plus d'information</a>
+              </div>
+              <div class="card-footer">
+                <a class="white-text" href="https://www.cnil.fr/" target="_blank">Plus d'information</a>
+              </div>
             </div>
           </div>
-        </div>
+        </form>
       </div>
+    </div>
+  </div>
 
 
-<?php } ?>
-    </main>
-<?php include("includes/footer.php"); ?>
+<?php }
+include("includes/footer.php"); ?>
   </body>
 </html>
