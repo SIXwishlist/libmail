5d4b676f12a9652b93ccd4cedbc6e4933f22e1c5
dpress29 <dpressense@protonmail.com>
2018-01-11 22:11:17 +0100

    minor fix

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="topics" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Liens</a>
             <div class="dropdown-menu" aria-labelledby="topics">
-            <a class="dropdown-item" href="https://laquadrature.net">La quadrature  du net</a>
+            <a class="dropdown-item" href="https://laquadrature.net/fr">La quadrature  du net</a>
             <a class="dropdown-item" href="https://fsf.org">Free Software Foundation</a>
           </div>
         </li>
