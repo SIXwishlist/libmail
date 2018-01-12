bec1b4051e01ae43b8900dbb37bf4f7b1dd849a8
dpress29 <dpressense@protonmail.com>
2018-01-13 00:34:11 +0100

    enlarging navbar

--- includes/header.php
+++ includes/header.php
@@ -1,27 +1,31 @@
-  <nav class="navbar navbar-expand-lg fixed-top navbar-dark" style="background-color: #616161;">
-    <a class="navbar-brand" href="/">LIBMAIL</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav" aria-controls="nav" aria-expandeded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="nav">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="topics" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Liens</a>
-            <div class="dropdown-menu" aria-labelledby="topics">
-            <a class="dropdown-item" href="https://laquadrature.net/fr">La quadrature  du net</a>
-            <a class="dropdown-item" href="https://fsf.org">Free Software Foundation</a>
-          </div>
-        </li>
-        <li class="nav-item">
-            <a href="https://wiki.libmail.eu" class="nav-link">Wiki</a>
-        </li>
-      <li class="nav-item">
-          <a href="blog" class="nav-link">Blog</a>
-        </li>
+    <nav class="navbar navbar-expand-lg fixed-top navbar-dark" style="background-color: #616161;">
+      <div class="container">
+        <a class="navbar-brand" href="/">
+          <h1 class="brand-link">LIBMAIL</h1>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav" aria-controls="nav" aria-expandeded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="nav">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="topics" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Liens</a>
+                <div class="dropdown-menu" aria-labelledby="topics">
+                <a class="dropdown-item" href="https://laquadrature.net/fr">La quadrature  du net</a>
+                <a class="dropdown-item" href="https://fsf.org">Free Software Foundation</a>
+              </div>
+            </li>
+            <li class="nav-item">
+                <a href="https://wiki.libmail.eu" class="nav-link">Wiki</a>
+            </li>
           <li class="nav-item">
-          <a href="https://searx.libmail.eu" class="nav-link">Searx</a>
-        </li>
-      </ul>
-      <a class="btn btn-primary" href="https://web.libmail.eu">Mon compte</a>
-    </div>
-  </nav>
+              <a href="blog" class="nav-link">Blog</a>
+            </li>
+              <li class="nav-item">
+              <a href="https://searx.libmail.eu" class="nav-link">Searx</a>
+            </li>
+          </ul>
+          <a class="btn btn-primary" href="https://web.libmail.eu">Mon compte</a>
+        </div>
+      </div>
+    </nav>
