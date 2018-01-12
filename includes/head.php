0bff48844c44d2c328ef1607c5bd09473b60be21
dpress29 <dpressense@protonmail.com>
2018-01-13 00:37:00 +0100

    update of css padding parameters

--- includes/head.php
+++ includes/head.php
@@ -11,8 +11,8 @@
         min-height: 100%;
       }
       body {
-        padding-top: 4.5rem; /*for navbar*/
-        padding-bottom: 60px; /*for footer*/
+        padding-top: 6.5rem; /*for navbar*/
+        padding-bottom: 5rem; /*for footer*/
       }
       .footer {
         position: absolute;
@@ -25,9 +25,9 @@
       .padding {
         padding: 4.5%;
       }
+      .brand-link {
+        padding-top: 0.5rem
+      }
     </style>
   </head>
   <body>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
