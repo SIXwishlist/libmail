1c8746c87b8c8f32c3e37b5bf6da02ce3360b9e1
dpress29 <dpressense@protonmail.com>
2018-01-25 20:22:20 +0100

    link color change

--- includes/header.php
+++ includes/header.php
@@ -9,20 +9,20 @@
         <div class="collapse navbar-collapse" id="nav">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="topics" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Liens</a>
+              <a class="nav-link text-light dropdown-toggle" href="#" id="topics" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Liens</a>
                 <div class="dropdown-menu" aria-labelledby="topics">
                 <a class="dropdown-item" href="https://laquadrature.net/fr">La quadrature  du net</a>
                 <a class="dropdown-item" href="https://fsf.org">Free Software Foundation</a>
               </div>
             </li>
             <li class="nav-item">
-                <a href="https://wiki.libmail.eu" class="nav-link">Wiki</a>
+                <a href="https://wiki.libmail.eu" class="nav-link text-light">Wiki</a>
             </li>
           <li class="nav-item">
-              <a href="blog" class="nav-link">Blog</a>
+              <a href="blog" class="nav-link text-light">Blog</a>
             </li>
               <li class="nav-item">
-              <a href="https://searx.libmail.eu" class="nav-link">Searx</a>
+              <a href="https://searx.libmail.eu" class="nav-link text-light">Searx</a>
             </li>
           </ul>
           <a class="btn btn-warning" href="https://web.libmail.eu">Mon compte</a>
