4b4d9ccd729cee47dcbbc6db6f217407a16335d0
dpress29 <dpressense@protonmail.com>
2018-01-25 12:50:41 +0100

    update footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,7 @@
     <footer class="footer">
       <div class="container">
-        <span class="text-muted">Copyright <a class="text-warning" href="https://github.com/dpress29/libmail/graphs/contributors">Team Libmail</a></span>
+        <span>Made by <a class="text-warning" href="https://github.com/dpress29/libmail/graphs/contributors">Team Libmail</a> under license <a href="https://github.com/dpress29/libmail/LICENSE.md">GNU GPL v3</a></span>
+	<span><a href="https://github.com/dpress29/libmail/">Voir le code source</a></span>
       </div>
     </footer>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
